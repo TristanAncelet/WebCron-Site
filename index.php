9e6369d136f1f46704352879846eb2284be80183
Tristan Ancelet <tristanancelet@yahoo.com>
2023-05-06 20:05:15 -0500

    test commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!Doctype html>
+<!-- 
+    Color pallet: https://colorhunt.co/palette/b9eddd87cbb9569daa577d86 
+-->
+<html>
+    <head>
+        <link rel="sytlesheet" href="css/main.css">
+        <title>This is a test</title>
+    </head>
+    </body>
+        <nav>
+            This is a test
+        </nav>
+        <?php
+        $a = 42;
+
+        echo "The number is $a"
+        ?>
+    </body>
+</html>
